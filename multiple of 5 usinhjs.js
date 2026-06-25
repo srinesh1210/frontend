ab97deb15f7186c3401ca0a6443a03ad93d16bde
srinesh1210 <vangalasrineshgoud@gmail.com>
2026-06-25 15:31:54 +0530

    Add check for multiples of 5 in JavaScript

---
+++ multiple of 5 usinhjs.js
@@ -0,0 +1,18 @@
+// console.log("srinesh");
+// console.log("my latop server")
+// let num=10;
+// if(num%2===0){
+//     console.log(num,"is even");
+// }
+// else{
+//     console.log(num,"is odd");
+// }
+// let name=prompt("hello");   //promt does the wordings of it to tkae user input in it 
+// console.log(name);
+let num=prompt("enter the number :");
+if(num%5==0){
+    console.log(num," is multiple of 5");
+}
+else{
+    console.log(num,"not multiple of 5");
+}
