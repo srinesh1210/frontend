c7319203ac2b1bf43d678ed5c9f47772d33ff514
srinesh1210 <vangalasrineshgoud@gmail.com>
2026-06-28 21:13:02 +0530

    Add number guessing game implementation

---
+++ guess number.js
@@ -0,0 +1,6 @@
+let num=67;
+let usernum=prompt("guess the number by entering:");
+while(usernum!=num){
+    usernum=prompt("you enetered the wrong number.guess again:");
+}
+console.log("congaratulatins, you have guees the right number");
